ff8e40f3608965eee06b933174e22f69fb3e068d
StrJSalmela <jennamaikkunensalmela@hotmail.com>
2026-02-10 21:22:11 +0100

    Datainmatning med while-loop

--- Datainmatning.py
+++ Datainmatning.py
@@ -1,1 +1,8 @@
-print("Nu testar vi")
+summa = 0
+
+while summa < 100:
+    tal = int(input("Mata in ett tal: "))
+    summa += tal
+    print("Nuvarande summa:", summa)
+if summa >= 100: 
+    print("Du har nått gränsen!")
