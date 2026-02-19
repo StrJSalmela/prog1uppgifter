f93c99b79c3f064a7b609ddec8670ebd5877f445
StrJSalmela <jennamaikkunensalmela@hotmail.com>
2026-02-19 21:40:50 +0100

    Rättade fel med kommentarer

---
+++ Felsöka och rätta.py
@@ -0,0 +1,13 @@
+# Skapa en lista med tal
+numbers = [10, 20, 30, 40, 50] #hakparentes saknades, listan blev inte stängd (syntaxfel)
+
+# Räkna ut summan av talen
+total = 0
+for num in numbers: #kolon saknades (syntaxfel)
+    total = total + num
+
+# Beräkna medelvärdet
+average = total / len(numbers) #felstavad variabel, numberss ska vara numbers (körtidsfel)
+
+# Skriv ut resultatet
+print("Medelvärdet är: " + str(average)) #
