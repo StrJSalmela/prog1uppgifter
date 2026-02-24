48d71be3e5b29f39b810b9cdb121dd2c3c6be680
StrJSalmela <jennamaikkunensalmela@hotmail.com>
2026-02-24 10:35:55 +0100

    Senaste Ålderskontroll ink. try/except

--- Ålderskontroll.py
+++ Ålderskontroll.py
@@ -1,6 +1,9 @@
-ålder = int(input("Hur gammal är du? "))
+try:
+    ålder = int(input("Hur gammal är du? "))
 
-if ålder >= 18:
-    print("Du är myndig!")
-else: 
-    print("Du är omyndig!")
+    if ålder >= 18:
+        print("Du är myndig!")
+
+    else: 
+        print("Du är omyndig!")
+except ValueError: print("Använd heltal vid inmatning, prova igen!")
